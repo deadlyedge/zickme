69e45ce7446ddfb883f75b50336859024b653760
deadlyedge <xdream@gmail.com>
2021-03-29 16:33:47 +0800

    clean codes

--- service.py
+++ service.py
@@ -21,7 +21,6 @@ bootstrap = Bootstrap(app)
 def getImageCode(imageFile):
     fileString = imageFile  # .read() if it's a file and not a string
     imgArray = np.frombuffer(fileString, np.uint8)  # change from 'fromstring'
-    # image = cv2.imdecode(imgArray, cv2.IMREAD_COLOR)
     imageGray = cv2.imdecode(imgArray, cv2.COLOR_RGB2GRAY)
     orb = cv2.ORB_create(SAMPLE_POINTS)
     keypoints, descriptors = orb.detectAndCompute(imageGray, None)
@@ -78,25 +77,6 @@ def index():
             with urlopen(picture) as response:
                 picture = response.read()
             if matchWithDB(readCode, getImageCode(picture)[1]):
-                # localImage = Image.fromarray(processImage[2])
-                # fontPath = 'data/font/msyh.ttc'
-                # font = ImageFont.truetype(fontPath, 24)
-                # b, g, r, a = 50, 50, 200, 0
-                # text = '\n'.join(textwrap.wrap(readBack['words'], width=15))
-                # textWidth, textHeight = font.getsize_multiline(text)
-                # draw = ImageDraw.Draw(localImage, 'RGBA')
-                # draw.rectangle((30, 40, textWidth * 1.2, textHeight * 1.44), fill=(180, 180, 180, 160))
-                # draw.multiline_text(
-                #     (50, 50),
-                #     text,
-                #     font=font, fill=(b, g, r, a)
-                # )
-                # textImage = np.array(localImage)
-                # cv2.putText(
-                #     localImage, readBack['words'], (10, 50),
-                #     cv2.FONT_HERSHEY_SIMPLEX, 2, (200, 50, 50), 1
-                # )
-                # cv2.imwrite('textImage.jpg', textImage)
                 return readBack["words"]
             else:
                 return '图片密码不匹配'
